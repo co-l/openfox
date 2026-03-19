9139e955ba54794ec341852ff9c74fe66e503eb1
conrad <c.lelubre@gmail.com>
2026-03-19 21:33:27 +0200

    fix: serve favicon in production and show dev title in dev mode

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
 
+if (import.meta.env.DEV) document.title = 'dev-OpenFox'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
